5551395c9418cf8989199858b4f262ad49e273f9
Jarosław Świderski <jarekswiderski2@gmail.com>
2023-06-22 21:02:35 +0200

    cors 2 refactor

--- RentCarsAPI/Startup.cs
+++ RentCarsAPI/Startup.cs
@@ -57,7 +57,7 @@ namespace RentCarsAPI
                 
                     builder.AllowAnyMethod()
                             .AllowAnyHeader()
-                            .AllowAnyHeader()
+                            .AllowAnyOrigin()
                 );
             });
         }
